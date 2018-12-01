089b0555f147c0da0b2a64676f8c4dd59a40eb9f
Justin Adkins <justin.adkins@mythcoders.com>
2018-11-30 22:53:13 -0500

    Mail logs in the UI

--- app/models/mail_log.rb
+++ app/models/mail_log.rb
@@ -1,5 +1,17 @@
 class MailLog < ApplicationRecord
   belongs_to :client
 
-  validates_presence_of :to, :subject
+  validates_presence_of :to, :subject, :client
+
+  def formatted_to
+    to.split(',')
+  end
+
+  def formatted_cc
+    cc.split(',')
+  end
+
+  def formatted_bcc
+    bcc.split(',')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   end
 
   resources :clients
+  resources :mail_logs
 end
