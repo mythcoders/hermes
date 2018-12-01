2524e1a56a9b1dc410b72c7270dd3e19ed14d27a
Justin Adkins <justin.adkins@mythcoders.com>
2018-11-30 23:08:35 -0500

    Start on api stuff

---
+++ lib/hermes/api_error.rb
@@ -0,0 +1,21 @@
+module Hermes
+  class APIError < StandardError
+
+    def initialize(status, message, http_method, rest_resource)
+      @status = status
+      @error_json = message
+      @http_method = http_method
+      @rest_resource = rest_resource
+    end
+
+    def message
+      <<-EOS.gsub(/^ {10}/, "")
+        #{@status}: #{@error_json}
+        in response to:
+        #{@http_method.upcase} #{@rest_resource}
+      EOS
+    end
+
+    attr_reader :status, :error_json, :http_method, :rest_resource
+  end
+end
