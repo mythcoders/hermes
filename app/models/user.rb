dc7d080a87211f30ff594cfe42b7b13d66d9b117
Justin Adkins <justin.adkins@mythcoders.com>
2019-01-25 00:50:59 -0500

    Users can only belong to MythCoders

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :confirmable,
          :lockable, :timeoutable
+
+  validate :email_domain_is_mythcoders
+
+  def email_domain
+    email.split('@')[1]
+  end
+
+  private
+
+  def email_domain_is_mythcoders
+    errors.add(:email, 'Invalid domain') if email_domain != 'mythcoders.com'
+  end
 end
