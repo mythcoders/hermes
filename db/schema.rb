cbe12aa507b697320b44b98d0ad9e9ffc2088a1d
Justin Adkins <justin.adkins@mythcoders.com>
2020-09-15 01:34:16 -0400

    Remove default columsn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_033340) do
+ActiveRecord::Schema.define(version: 2020_09_15_051422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 2020_09_15_033340) do
 
   create_table "client_environments", force: :cascade do |t|
     t.bigint "client_id", null: false
-    t.string "name", default: "f", null: false
-    t.string "status", default: "f", null: false
+    t.string "name", null: false
+    t.string "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["client_id"], name: "index_client_environments_on_client_id"
