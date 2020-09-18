dedd59accb14849013a3d289b413e8c22786e5a6
Justin Adkins <justin.adkins@mythcoders.com>
2020-09-18 13:42:20 -0400

    Processing of client uploads!

--- app/models/client_upload.rb
+++ app/models/client_upload.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ClientUpload < ApplicationRecord
+  include AASM
+  after_create :process_upload
+
   belongs_to :client, inverse_of: :uploads
   has_one_attached :data_file
 
@@ -9,4 +12,27 @@ class ClientUpload < ApplicationRecord
   enum file_type: {
     subscriber: 'subscriber'
   }
+
+  aasm column: :status do
+    state :created, initial: true
+    state :running, :errored, :finished
+
+    event :start do
+      transitions from: %i[created], to: :running
+    end
+
+    event :error do
+      transitions from: %i[running created], to: :errored
+    end
+
+    event :finish do
+      transitions from: %i[running errored], to: :finished
+    end
+  end
+
+  private
+
+  def process_upload
+    ClientUploadWorker.perform_async id
+  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@
 
 class Subscription < ApplicationRecord
   belongs_to :topic, class_name: 'MailingTopic', foreign_key: 'mailing_topic_id'
-  belongs_to :subscriber
+  belongs_to :subscriber, autosave: true
 
   enum status: {
     inactive: 0,
