782d308089d457f75de0008870e5022f253bdb90
Justin Adkins <justin.adkins@mythcoders.com>
2020-09-22 01:07:36 -0400

    User fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     email { "#{Faker::Lorem.characters(number: 5)}@mythcoders.com" }
-    password { Faker::Lorem.characters(number: 10) }
+    password { Faker::Lorem.characters(number: 15) }
     name { Faker::Movies::HarryPotter.character }
   end
 end
