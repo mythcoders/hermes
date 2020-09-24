a0a08dc42308de5c9f1bd057c887f298ef51acca
Justin Adkins <justin.adkins@mythcoders.com>
2020-09-24 01:09:44 -0400

    Send each recipient individually

--- app/workers/postal_worker.rb
+++ app/workers/postal_worker.rb
@@ -9,15 +9,31 @@ class PostalWorker
 
     return if message.sent?
 
-    # TODO: Batch messages up by a max of 50 recipients
+    message.recipients.each do |r|
+      ApiMailer.with(message: build_params(message, r))
+               .send_message
+               .deliver_now
+    end
 
-    ApiMailer.with(message: message).send_message.deliver_now
     message.processed!
   end
 
   private
 
   def message
-    @message ||= Message.find_by_tracking_id @tracking_id
+    @message ||= Message.includes(:recipients).find_by_tracking_id @tracking_id
+  end
+
+  def build_params(message, recipient)
+    OpenStruct.new(
+      from: message.sender ||= ApplicationMailer::DEFAULT_FROM,
+      html_body: message.html_body,
+      priority: message.priority,
+      # reply_to: ApplicationMailer::DEFAULT_REPLY,
+      subject: message.subject,
+      text_body: message.text_body,
+      to: recipient.email,
+      tracking_id: message.tracking_id
+    )
   end
 end
