eb034725c89b838026802c34d7d43aece1789953
Justin Adkins <justin@mythcoders.com>
2024-09-30 22:21:03 -0400

    Rails 8 upgrade

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 ruby file: ".ruby-version"
 
-gem "rails", "~> 7.2"
+gem "rails", "~> 8.0.0.beta"
 
 gem "activerecord-enhancedsqlite3-adapter", "~> 0.8.0"
 gem "bcrypt"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_14_202100) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_01_021561) do
   create_table "activities", force: :cascade do |t|
     t.integer "recipient_id", null: false
     t.string "action_type"
